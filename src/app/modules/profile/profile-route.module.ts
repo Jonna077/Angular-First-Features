ad82d6b9d9e3eb47f6278b7d54ec4de1353f2312
Jonna077 <jonnamaemayang7@gmail.com>
2023-01-10 19:37:10 +0800

    final commit

--- src/app/modules/profile/profile-route.module.ts
+++ src/app/modules/profile/profile-route.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
 
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
-  ]
+  ],
+  exports: [RouterModule]
 })
 export class ProfileRouteModule { }
